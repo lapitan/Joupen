8c87eada04f9c947ca79b2a267efe2c958ca0035
lapitaniy <lapitaniy@gmail.com>
2024-04-22 22:07:25 +0300

    Initial Commit

--- src/main/java/org/joutak/loginpluginforjoutak/LoginPluginForJoutak.java
+++ src/main/java/org/joutak/loginpluginforjoutak/LoginPluginForJoutak.java
@@ -6,7 +6,7 @@ public final class LoginPluginForJoutak extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        System.out.println("123");
 
     }
 
